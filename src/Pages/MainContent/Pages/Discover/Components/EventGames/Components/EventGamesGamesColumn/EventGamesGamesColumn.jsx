34cd36faf644e1e185233e14d84897c39d93fe68
Iqbal Hossain Emon <iqbalhossainemon2@gmail.com>
2024-03-11 05:04:49 +0000

    (discover) banner margin top fixed

--- src/Pages/MainContent/Pages/Discover/Components/EventGames/Components/EventGamesGamesColumn/EventGamesGamesColumn.jsx
+++ src/Pages/MainContent/Pages/Discover/Components/EventGames/Components/EventGamesGamesColumn/EventGamesGamesColumn.jsx
@@ -1,7 +1,10 @@
+import { useRef } from 'react';
+import ButtonWaterEffect from '../../../../../../../../Shared/ButtonWaterEffect/ButtonWaterEffect';
 import GameInColumn from '../EventGamesGameColumn/EventGamesGameInColumn';
 import styles from './EventGamesGamesColumn.module.css';
 
 export default function EventGamesGamesColumn({ header, games, border, screenWidth, colNum, cardPosition }) {
+  const btnRef = useRef(null);
   return (
     <li
       className={colNum === cardPosition ? styles.GameColumn : [styles.GameColumn, styles.Opacity0].join(' ')}
@@ -9,7 +12,10 @@ export default function EventGamesGamesColumn({ header, games, border, screenWid
     >
       <div className={styles.heading}>
         <h2>{header}</h2>
-        <button type="button">View More</button>
+        <button ref={btnRef} type="button">
+          View More
+          <ButtonWaterEffect btnRef={btnRef} />
+        </button>
       </div>
       <ul className={`${border && styles.rightBorder} ${styles.column}`}>
         {games.map(game => (
