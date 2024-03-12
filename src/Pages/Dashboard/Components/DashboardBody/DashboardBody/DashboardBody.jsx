d1032b76a8b13e1a4f5011e657d359b2a6f798ac
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-03-13 03:32:35 +0600

    (scrollbar) finished

--- src/Pages/Dashboard/Components/DashboardBody/DashboardBody/DashboardBody.jsx
+++ src/Pages/Dashboard/Components/DashboardBody/DashboardBody/DashboardBody.jsx
@@ -1,11 +1,21 @@
+import { useRef } from 'react';
+import ScrollBar from '../../../../../Shared/ScrollBar/ScrollBar';
 import AddGame from '../Components/AddGame/AddGame/AddGame';
 import styles from './DashboardBody.module.css';
 
-const DashboardBody = () => (
-  <div className={styles.dashboardBodyContainer}>
-    <div className={styles.dashboardBody}>
-      <AddGame />
+const DashboardBody = () => {
+  const parentRef = useRef(null);
+  const childRef = useRef(null);
+
+  return (
+    <div className={styles.dashboardBodyScrollContainer}>
+      <div ref={parentRef} className={styles.dashboardBodyContainer}>
+        <div ref={childRef} className={styles.dashboardBody}>
+          <AddGame />
+        </div>
+      </div>
+      <ScrollBar parentRef={parentRef} childRef={childRef} />
     </div>
-  </div>
-);
+  );
+};
 export default DashboardBody;
--- src/Shared/ScrollBar/ScrollBar.jsx
+++ src/Shared/ScrollBar/ScrollBar.jsx
@@ -9,14 +9,37 @@ const ScrollBar = ({ parentRef, childRef }) => {
 
   const thumbRef = useRef(null);
   const downElement = useRef(null);
+  const containerRef = useRef(null);
+  const timerID = useRef(null);
 
-  const handleMove = useCallback(e => {
-    const cursorInEle = e?.touches
-      ? e.touches[0].pageY - thumbRef.current.getBoundingClientRect().y
-      : e.pageY - thumbRef.current.getBoundingClientRect().y;
+  const handleMove = useCallback(
+    e => {
+      const cursorInEle = e?.touches
+        ? e.touches[0].pageY - thumbRef.current.getBoundingClientRect().y
+        : e.pageY - thumbRef.current.getBoundingClientRect().y;
 
-    downElement.current = cursorInEle;
-  }, []);
+      if (!downElement.current) {
+        downElement.current = cursorInEle;
+      }
+
+      setScrolled(prev => {
+        if (prev + cursorInEle - downElement.current < 0) {
+          parentRef.current.scrollTop = 0;
+          return 0;
+        }
+        if (prev + cursorInEle - downElement.current > parentRef.current.clientHeight - thumbRef.current.clientHeight) {
+          parentRef.current.scrollTop = parentRef.current.scrollHeight - parentRef.current.clientHeight;
+          return parentRef.current.clientHeight - thumbRef.current.clientHeight;
+        }
+
+        parentRef.current.scrollTop =
+          ((prev + cursorInEle - downElement.current) / (parentRef.current.clientHeight - thumbRef.current.clientHeight)) *
+          (parentRef.current.scrollHeight - parentRef.current.clientHeight);
+        return prev + cursorInEle - downElement.current;
+      });
+    },
+    [parentRef]
+  );
 
   const handleMouseUp = useCallback(() => {
     downElement.current = null;
@@ -24,19 +47,64 @@ const ScrollBar = ({ parentRef, childRef }) => {
 
   const onStart = useDragStartStop(handleMove, handleMouseUp);
 
+  const handleScrollHide = () => {
+    if (timerID.current) {
+      clearTimeout(timerID.current);
+      timerID.current = null;
+    }
+    timerID.current = setTimeout(() => {
+      timerID.current = null;
+      setShow(false);
+    }, 2000);
+  };
+
   useEffect(() => {
-    setHeight((parentRef.current.scrollHeight / childRef.current.scrollHeight) * 100);
+    window.addEventListener('resize', () => {
+      setHeight(() => {
+        const heightCheck = (parentRef.current.clientHeight / parentRef.current.scrollHeight) * 100;
+
+        if (heightCheck > 100) {
+          return 100;
+        }
+        return heightCheck;
+      });
+    });
   }, [childRef, parentRef]);
 
-  return !parentRef || !childRef || childRef.current?.scrollHeight <= parentRef.current?.scrollHeight ? null : (
-    <div className={styles.scrollBarContainer}>
+  useEffect(() => {
+    new ResizeObserver(() => {
+      setHeight(() => {
+        const heightCheck = (parentRef.current.clientHeight / parentRef.current.scrollHeight) * 100;
+        console.log(heightCheck);
+        if (heightCheck > 100) {
+          return 100;
+        }
+        return heightCheck;
+      });
+    }).observe(childRef.current);
+    parentRef.current.addEventListener('scroll', () => {
+      setShow(true);
+      handleScrollHide();
+      setScrolled(
+        (parentRef.current.scrollTop / (parentRef.current.scrollHeight - parentRef.current.clientHeight)) *
+          (parentRef.current.clientHeight - thumbRef.current.clientHeight)
+      );
+    });
+    containerRef.current.addEventListener('mousemove', () => {
+      setShow(true);
+      handleScrollHide();
+    });
+  }, [childRef, parentRef]);
+
+  return (
+    <div ref={containerRef} className={`${height <= 0 || height >= 100 ? `${styles.noHeight} ` : ''}${styles.scrollBarContainers}`}>
       <button
         ref={thumbRef}
         onMouseDown={onStart}
         onTouchStart={onStart}
         type="button"
-        style={{ height: `${height}%` }}
-        className={styles.scrollThumb}
+        style={{ height: `${height}%`, translate: `0 ${scrolled < 0 ? 0 : scrolled}px` }}
+        className={`${show ? `${styles.show} ` : ''}${styles.scrollThumb}`}
       />
     </div>
   );
