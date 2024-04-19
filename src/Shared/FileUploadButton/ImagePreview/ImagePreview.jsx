78770a6398e20a94c6b4c0c0da9a53dc4aeb2fe9
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-04-19 16:59:27 +0600

    (fileUploadButton-imagePreview) structure donw

--- src/Shared/FileUploadButton/ImagePreview/ImagePreview.jsx
+++ src/Shared/FileUploadButton/ImagePreview/ImagePreview.jsx
@@ -1,30 +1,80 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './ImagePreview.module.css';
 
 const ImagePreview = ({ containerRef, file }) => {
-    const imgRef = useRef(null);
+    const [show, setShow] = useState(false);
+    const [hideAnimation, setHideAnimation] = useState(false);
+
+    const heightRef = useRef(null);
+    const imagePreviewRef = useRef(null);
+    const timeId = useRef(null);
+    const imageRef = useRef(null);
+    const srcRef = useRef(null);
 
     useEffect(() => {
-        imgRef.current.src = URL.createObjectURL(file);
-    }, [file]);
+        if (!file) return;
+        srcRef.current = URL.createObjectURL(file);
+        if (!imageRef.current) {
+            imageRef.current = new Image();
+        }
+        imageRef.current.src = URL.createObjectURL(file);
+        imageRef.current.onload = () => {
+            // if container height more than imageRef than set height to imageRef height
+            let height;
+            if (imageRef.current.width + 32 > containerRef.current.clientWidth) {
+                height = `${(imageRef.current.height / imageRef.current.width) * (containerRef.current.clientWidth - 32)}px`;
+            } else {
+                height = `${imageRef.current.height}px`;
+            }
+            if (imagePreviewRef.current) {
+                imagePreviewRef.current.style.setProperty('--height', height);
+                heightRef.current = height;
+            } else {
+                heightRef.current = height;
+            }
+        };
+    }, [containerRef, file]);
 
     const handleHover = () => {
-        imgRef.current.style.transform = 'scale(1.1)';
+        if (timeId.current) return;
+        setShow(true);
+        setHideAnimation(false);
+    };
+
+    const handleLeave = () => {
+        if (timeId.current) {
+            clearTimeout(timeId.current);
+            timeId.current = null;
+        }
+        // setHideAnimation(true);
+        timeId.current = setTimeout(() => {
+            // setShow(false);
+            timeId.current = null;
+        }, 200);
     };
 
     useEffect(() => {
         const container = containerRef.current;
-        container.addEventListener('hover', handleHover);
-
+        container.addEventListener('mouseover', handleHover);
+        container.addEventListener('mouseleave', handleLeave);
         return () => {
-            container.removeEventListener('hover', handleHover);
+            container.removeEventListener('mouseover', handleHover);
+            container.removeEventListener('mouseleave', handleLeave);
         };
     }, [containerRef]);
 
-    return (
-        <div className={styles.imagePreview}>
-            <img className={styles.img} ref={imgRef} alt="preview" />
+    return show ? (
+        <div
+            ref={imagePreviewRef}
+            style={{
+                '--height': heightRef.current || '100%',
+            }}
+            className={`${styles.imagePreview}${hideAnimation ? ` ${styles.hide}` : ''}`}
+        >
+            <div className={styles.imgContainer}>
+                <img ref={imageRef} className={styles.img} src={srcRef.current} alt="preview" />
+            </div>
         </div>
-    );
+    ) : null;
 };
 export default ImagePreview;
