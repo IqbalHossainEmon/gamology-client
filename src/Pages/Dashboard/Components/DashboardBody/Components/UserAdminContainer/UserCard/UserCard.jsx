fa65d3ff7b2e47c6f7a94899a10eeb0da0984af9
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-06-20 16:18:07 +0600

    users demo added

--- src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/UserCard/UserCard.jsx
+++ src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/UserCard/UserCard.jsx
@@ -1,14 +1,15 @@
 import styles from './UserCard.module.css';
 
 const UserCard = ({ data }) => {
-    const { img, name } = data;
+    const { img, name, email } = data;
     return (
         <div className={styles.cardContainer}>
             <div className={styles.cardContainerImg}>
                 <img src={img} alt={name} />
             </div>
-            <div className={styles.cardContainerName}>
+            <div className={styles.cardContainerInfo}>
                 <h4>{name}</h4>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/UserContainer/UserContainer/UserContainer.jsx
+++ src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/UserContainer/UserContainer/UserContainer.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react';
 import UserCard from '../../UserCard/UserCard';
 import styles from './UserContainer.module.css';
 
-const UserContainer = () => {
-    const userDetail = {
+const userDetail = [];
+for (let i = 0; i < 100; i++) {
+    const user = {
+        id: i,
         img: '/assets/images/user-1.png',
-        name: 'John Doe',
+        name: `User ${i}`,
+        email: `user${i}@example.com`,
     };
+    userDetail.push(user);
+}
+
+const UserContainer = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        setUsers(userDetail);
+    }, []);
+
     return (
         <div className={styles.userContainer}>
-            <UserCard data={userDetail} />
+            {users.map(user => (
+                <UserCard key={user.id} data={user} />
+            ))}
         </div>
     );
 };
