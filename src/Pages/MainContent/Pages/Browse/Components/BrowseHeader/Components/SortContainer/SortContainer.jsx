4ed1295924e02b7c4347a935d447291603d63aa8
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-06-21 18:40:18 +0600

    refactor: Update FilterSortContext and SortContainer components

--- src/Pages/MainContent/Pages/Browse/Components/BrowseHeader/Components/SortContainer/SortContainer.jsx
+++ src/Pages/MainContent/Pages/Browse/Components/BrowseHeader/Components/SortContainer/SortContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import useScreenWidth from '../../../../../../../../Hooks/useScreenWidth';
 import ScreenShadow from '../../../../../../../../Shared/ScreenShadow/ScreenShadow';
 import useFilterSortState from '../../../FilterGames/Components/useFilterSortState/useFilterSortState';
@@ -9,7 +9,60 @@ import styles from './SortContainer.module.css';
 
 export default function SortContainer({ state, handleChange }) {
     const { filterSortState, setFilterSort, filterSortRef } = useFilterSortState();
-    const { sort: show, fadeIn } = filterSortState;
+    const { sort } = filterSortState;
+
+    const [show, setShow] = useState(false);
+    const [fadeIn, setFadeIn] = useState(false);
+
+    const startTimeRef = useRef();
+    const endTimeRef = useRef();
+
+    const handleHideBtn = useCallback(() => {
+        if (startTimeRef.current) {
+            clearTimeout(startTimeRef.current);
+            startTimeRef.current = null;
+            setFadeIn(false);
+            return;
+        }
+        if (endTimeRef.current) {
+            return;
+        }
+        setFadeIn(false);
+        endTimeRef.current = setTimeout(() => {
+            setShow(false);
+            window.removeEventListener('blur', handleHideBtn);
+            endTimeRef.current = null;
+        }, 200);
+    }, []);
+
+    const handleShow = useCallback(() => {
+        if (endTimeRef.current) {
+            clearTimeout(endTimeRef.current);
+            endTimeRef.current = null;
+            setFadeIn(true);
+            return;
+        }
+        if (startTimeRef.current) {
+            return;
+        }
+        setShow(true);
+        startTimeRef.current = setTimeout(() => {
+            setFadeIn(true);
+            window.addEventListener('blur', handleHideBtn);
+            startTimeRef.current = null;
+        }, 60);
+    }, [handleHideBtn]);
+
+    useEffect(() => {
+        switch (sort) {
+            case true:
+                handleShow();
+                break;
+            default:
+                handleHideBtn();
+                break;
+        }
+    }, [handleHideBtn, handleShow, sort]);
 
     const dropDownRef = useRef();
 
