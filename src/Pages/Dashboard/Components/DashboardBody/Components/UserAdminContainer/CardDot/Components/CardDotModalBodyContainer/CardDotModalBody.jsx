30e451727f6266b3bd58ae12a84c9e64ad60afdf
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-06-24 00:36:13 +0600

    (userAdminContainer) cardDot added

---
+++ src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/CardDot/Components/CardDotModalBodyContainer/CardDotModalBody.jsx
@@ -0,0 +1,31 @@
+import DashboardDeleteModal from '../DashboardDeleteModal/DashboardDeleteModal';
+import DashboardPriceChangeModal from '../DashboardPriceChangeModal/DashboardPriceChangeModal';
+import styles from './CardDotModalBody.module.css';
+
+const CardDotModalBody = ({ type, detail, handleHide }) => {
+    const { name, category, price, carouselThumb } = detail;
+    return (
+        <>
+            <div className={styles.card}>
+                <div className={styles.cardHeader}>
+                    <img src={carouselThumb} alt={name} />
+                </div>
+                <div className={styles.cardBody}>
+                    <p className={styles.name}>{name}</p>
+                    <p className={styles.category}>{category?.card}</p>
+                    <p className={styles.price}>$ {typeof price === 'object' ? price.regular : price}</p>
+                </div>
+            </div>
+            {type === 'price' ? (
+                <DashboardPriceChangeModal
+                    handleHide={handleHide}
+                    detail={detail}
+                    price={typeof price === 'object' ? price.regular : price}
+                />
+            ) : (
+                <DashboardDeleteModal detail={detail} handleHide={handleHide} />
+            )}
+        </>
+    );
+};
+export default CardDotModalBody;
---
+++ src/Pages/Dashboard/Components/DashboardBody/Components/UserAdminContainer/CardDot/Components/DashboardDeleteModal/DashboardDeleteModal.jsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react';
+import TextField from '../../../../../../../../../../../../../../Shared/TextField/TextField';
+import styles from './DashboardDeleteModal.module.css';
+
+const DashboardDeleteModal = ({ handleHide }) => {
+    const confirmText = useRef(null);
+
+    const [{ errorChange, errorMessage }, setError] = useState({ errorChange: 0, errorMessage: '' });
+
+    const handleDelete = () => {
+        if (confirmText.current === 'DELETE' || confirmText.current === 'delete' || confirmText.current === 'Delete') {
+            console.log('Deleted');
+            handleHide();
+        } else {
+            setError(prev => ({ errorChange: prev.errorChange + 1, errorMessage: 'Please type delete to confirm' }));
+        }
+    };
+
+    return (
+        <div className={styles.deleteModal}>
+            <TextField
+                errorChange={errorChange}
+                errorMessage={errorMessage}
+                type="text"
+                placeholder="Type 'DELETE' to confirm"
+                label="Type 'delete' to confirm"
+                field="input"
+                setState={val => {
+                    confirmText.current = val;
+                }}
+                className={styles.textField}
+            />
+            <button type="button" className={styles.deleteBtn} onClick={handleDelete}>
+                Delete
+            </button>
+        </div>
+    );
+};
+export default DashboardDeleteModal;
