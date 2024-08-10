534667483e18da853174f76f1d2f4f462f4d4910
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-08-11 05:11:31 +0600

    refector: update usedropdownhide

--- src/Hooks/useDropDownHide.js
+++ src/Hooks/useDropDownHide.js
@@ -1,55 +1,55 @@
-import { useCallback, useRef } from 'react';
+import { useRef } from 'react';
 
 const useDropDownHide = setState => {
-    const element = useRef();
-    const eventRefs = useRef({
-        closeMenu: () => {},
-        closeMenuBlur: () => {},
-    });
+	const element = useRef();
+	const eventRefs = useRef(null);
+	if (!eventRefs.current) {
+		eventRefs.current = {
+			closeMenu: e => {
+				switch (Array.isArray(element.current)) {
+					case true:
+						if (!element.current.some(ele => ele?.contains(e.target)) && e) {
+							document.removeEventListener('mousedown', eventRefs.current.closeMenu);
+							window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
+							setState(false);
+						}
+						break;
+					default:
+						if (element.current && e && !element.current.contains(e.target)) {
+							document.removeEventListener('mousedown', eventRefs.current.closeMenu);
+							window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
+							setState(false);
+						}
+						break;
+				}
+			},
 
-    eventRefs.current.closeMenu = useCallback(
-        e => {
-            switch (Array.isArray(element.current)) {
-                case true:
-                    if (!element.current.some(ele => ele?.contains(e.target)) && e) {
-                        document.removeEventListener('mousedown', eventRefs.current.closeMenu);
-                        window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
-                        setState(false);
-                    }
-                    break;
-                default:
-                    if (element.current && e && !element.current.contains(e.target)) {
-                        document.removeEventListener('mousedown', eventRefs.current.closeMenu);
-                        window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
-                        setState(false);
-                    }
-                    break;
-            }
-        },
-        [setState]
-    );
+			closeMenuBlur: () => {
+				setState(false);
+				window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
+				document.removeEventListener('mousedown', eventRefs.current.closeMenu);
+			},
 
-    eventRefs.current.closeMenuBlur = useCallback(() => {
-        setState(false);
-        window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
-        document.removeEventListener('mousedown', eventRefs.current.closeMenu);
-    }, [setState]);
+			stopMenu: () => {
+				document.removeEventListener('mousedown', eventRefs.current.closeMenu);
+				window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
+			},
 
-    const stopMenu = useCallback(() => {
-        document.removeEventListener('mousedown', eventRefs.current.closeMenu);
-        window.removeEventListener('blur', eventRefs.current.closeMenuBlur);
-    }, []);
+			setElement: ele => {
+				element.current = ele;
+			},
 
-    const setElement = useCallback(ele => {
-        element.current = ele;
-    }, []);
-
-    const showMenu = useCallback(() => {
-        document.addEventListener('mousedown', eventRefs.current.closeMenu);
-        window.addEventListener('blur', eventRefs.current.closeMenuBlur);
-    }, []);
-
-    return { showMenu, setElement, stopMenu };
+			showMenu: () => {
+				document.addEventListener('mousedown', eventRefs.current.closeMenu);
+				window.addEventListener('blur', eventRefs.current.closeMenuBlur);
+			},
+		};
+	}
+	return {
+		showMenu: eventRefs.current.showMenu,
+		setElement: eventRefs.current.setElement,
+		stopMenu: eventRefs.current.stopMenu,
+	};
 };
 
 export default useDropDownHide;
