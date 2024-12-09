2bfd48d17e8b748d53ac7825dbcb1ed52a0af5d9
IqbalHossainEmon <iqbalhossainemon2@gmail.com>
2024-12-10 05:19:02 +0600

    refector: exploreGames bars width calculated

--- src/Pages/MainContent/Pages/Discover/Components/ExploreGames/ExploreGames.jsx
+++ src/Pages/MainContent/Pages/Discover/Components/ExploreGames/ExploreGames.jsx
@@ -12,28 +12,39 @@ const fetched = {
 
 export default function ExploreGames() {
 	const buttonRef = useRef(null);
+	const containerRef = useRef(null);
 	const screenWidth = useScreenWidth();
 	const [data, setData] = useState({});
 
 	useEffect(() => {
 		setData(fetched);
+
+		const a = containerRef.current.getBoundingClientRect().width;
+		const b = containerRef.current.getBoundingClientRect().height;
+		const f = a * 0.10510423526567647;
+		const x = Math.ceil(Math.sqrt(a ** 2 + f ** 2));
+		const y = Math.ceil(Math.sqrt((a - x) ** 2 + (b - f) ** 2));
+
+		containerRef.current.style.setProperty('--bars-width', `${x}px`);
+		containerRef.current.style.setProperty('--bars-height', `${y}px`);
+
+		console.log(Math.sqrt(a ** 2 + f ** 2), a, y);
 	}, []);
 
 	return (
-		<section className={styles.ExploreGames}>
+		<section className={styles.exploreGames}>
 			<div
 				className={styles.exploreGamesBackground}
+				ref={containerRef}
 				style={
 					screenWidth >= 769
 						? { backgroundImage: `url(${fetched.backgroundDesktop})` }
 						: { backgroundImage: `url(${fetched.backgroundPhone})` }
 				}
 			>
-				<div className={styles.ExploreTexts}>
+				<div className={styles.exploreTexts}>
 					<h4>{data.heading}</h4>
-
 					<p>{data.details}</p>
-
 					<a href='#d' ref={buttonRef} type='button'>
 						Learn More
 						<ButtonWaterEffect backGround='white' btnRef={buttonRef} long />
